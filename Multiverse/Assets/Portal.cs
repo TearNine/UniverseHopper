2ae5230ef710cd5f70440e83bb8d8abc1b1d2ac7
Andreas Halldin <iqed@live.se>
2017-02-14 19:25:55 +0100

    init commit

---
+++ Multiverse/Assets/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public enum portalType { normal, lowGravity, highGravity };
+    public portalType type;
+
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        Rigidbody rb = c.gameObject.GetComponent<Rigidbody>();
+        switch (type)
+        {
+            case portalType.normal:
+                rb.mass = 1;
+                break;
+            case portalType.lowGravity:
+                rb.mass = .45f;
+                break;
+            case portalType.highGravity:
+                rb.mass = 50;
+                break;
+            default:
+                break;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Multiverse/Assets/PortalManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    Portal.portalType selectedPortal = Portal.portalType.normal;
+
+    public GameObject portalPrefab;
+
+    void SelectNormalPortal()
+    {
+        selectedPortal = Portal.portalType.normal;
+    }
+    void SelectLowGravityPortal()
+    {
+        selectedPortal = Portal.portalType.lowGravity;
+    }
+    void SelectHighGravityPortal()
+    {
+        selectedPortal = Portal.portalType.highGravity;
+    }
+
+}
